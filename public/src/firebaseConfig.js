e7ae53424bcef3367f23c56c1d5ebea40ca039e2
Jaisel Rahman <jaiselrahman@gmail.com>
2019-07-27 19:42:27 +0530

    Add Firebase config file

---
+++ public/src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA2UtLGKlTT-TD4Jt0lO2ghwZ4SYO0nWZo",
+    authDomain: "flaminius2k18-17019.firebaseapp.com",
+    databaseURL: "https://flaminius2k18-17019.firebaseio.com",
+    projectId: "flaminius2k18-17019",
+    storageBucket: "flaminius2k18-17019.appspot.com",
+    messagingSenderId: "356772075988",
+    appId: "1:356772075988:web:33c6f0613278941f"
+};
+
+export default firebaseConfig
